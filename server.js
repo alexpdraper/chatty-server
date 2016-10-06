b92b6dd8b55a457091fa2db3a88e339e5799de61
Alex <alexpdraper@gmail.com>
2016-10-06 19:28:47 +0000

    Added routing for messages and notifications

--- server.js
+++ server.js
@@ -30,8 +30,18 @@ wss.on('connection', (ws) => {
   ws.on('message', (message) => {
     const parsedMessage = JSON.parse(message);
     parsedMessage.id = uuid.v1();
-    console.log(`User ${parsedMessage.username} said ${parsedMessage.content}`);
-    wss.broadcast(JSON.stringify(parsedMessage));
+    switch(parsedMessage.type) {
+      case 'message':
+        console.log(`User ${parsedMessage.username} said ${parsedMessage.content}`);
+        wss.broadcast(JSON.stringify(parsedMessage));
+        break;
+      case 'notification':
+        console.log(`Notification: ${parsedMessage.content}`);
+        wss.broadcast(JSON.stringify(parsedMessage));
+        break;
+      default:
+        console.log('Received message:', parsedMessage);
+    }
   });
 
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
